319ba145bb18083bd4bf1fbc8cb19738c5d1ec65
tiagomartinho <t.martinho@icloud.com>
2019-09-25 13:19:40 +0200

    Initial Commit

---
+++ CustomizeNavigationBarBug/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomizeNavigationBarBug
+//
+//  Created by Martinho on 25/09/2019.
+//  Copyright © 2019 tm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
